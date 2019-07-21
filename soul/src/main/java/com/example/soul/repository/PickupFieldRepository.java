97852a9dba75eb8dd10e128d59acc18ce1217688
izzyMusa <izzy6174@gmail.com>
2019-07-20 19:25:20 -0500

    Revert "Added Pickup field object, service, repository, and controller"
    This reverts commit 52ae8af9840c2883e783a16c9ff595646a98b15f.

--- soul/src/main/java/com/example/soul/repository/PickupFieldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.soul.repository;
-
-import com.example.soul.model.PickupField;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PickupFieldRepository extends JpaRepository<PickupField, Integer> {
-}
--- soul/src/test/java/com/example/soul/controller/UserControllerSpec.groovy
+++
@@ -1,6 +0,0 @@
-package com.example.soul.controller
-
-import spock.lang.Specification
-
-class UserControllerSpec extends Specification {
-}
